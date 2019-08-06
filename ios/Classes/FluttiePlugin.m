da979e0d314ea87475c61170e5dd797c8505fdf6
Joseph <joseph.cheng@hmlet.com>
2019-08-06 14:37:53 +0800

    Try fix objc FluttiePlugin

--- ios/Classes/FluttiePlugin.m
+++ ios/Classes/FluttiePlugin.m
@@ -11,7 +11,7 @@
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"isAvailable" isEqualToString:call.method]) {
-    result([@NO]);
+    result(@(NO));
   } else {
     result(FlutterMethodNotImplemented);
   }
